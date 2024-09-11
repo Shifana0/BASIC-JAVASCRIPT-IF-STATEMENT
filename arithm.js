3cce5d075979ea40e2cc8f0b7c58d21020cb1edb
Fathima Shifana <shifana.4727@gmail.comgit config --global user.name>
2024-09-11 20:13:42 +0530

    BASIC JAVASCRIPT-IF STATEMENT

---
+++ arithm.js
@@ -0,0 +1,111 @@
+var a=10;
+var b=5;
+var sum=a+b;
+var diff=a-b;
+var product=a*b;
+var quotient=a/b;
+console.log("sum is",sum);
+console.log("difference is",diff);
+console.log("product is",product);
+console.log("quotient is",quotient);
+
+
+// console.log(2);
+var a="Fathima";
+var b=" Shifana";
+result=a+b;
+console.log(a.concat(b));
+
+
+// console.log(3);
+ temperature=50;
+if(temperature>30){
+    console.log("it's hot outside");
+}
+var number=4;
+if(number%2==0){
+    console.log("the number is even");
+}
+
+
+
+var age=18;
+if(age>=18){
+    console.log("you are an adult");
+}
+
+
+var x=10;
+var y=5;
+if(x>y){
+    console.log("x is greater than y");
+}else{
+    console.log("y is greater than x");
+}
+
+
+var length=10;
+var width=5;
+var area=length*width;
+console.log(area);
+if(area>20){
+    console.log(" the area is large");
+}
+
+
+var x=11;
+if(x>10){
+    console.log("greater than 10");
+}
+else{
+    console.log("less than 10");
+}
+
+
+
+var number=8;
+if(number%2==0){
+    console.log("even")
+}
+else if(number%2!=0){
+    console.log("odd");
+}
+
+
+var n=25;
+if(n%5==0){
+    console.log("multiple of 5");
+}
+else if(n%5!=0){
+    console.log("not multiple of 5");
+}
+
+
+var temp=-3;
+if(temp<0){
+    console.log("below freezing");
+}
+else{
+    console.log("above freezing");
+}
+
+
+
+var score=96;
+if(score>60){
+    console.log("pass");
+}
+else{
+    console.log("fail");
+}
+
+
+
+
+var age=12;
+if(age<13 && age<19){
+    console.log("teenager");
+}
+else{
+    console.log("not a teenager");
+}
